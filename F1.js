a06d014f86a8f46f3c938dfc1031ad557c2f4000
hingham <hannah.c.ingham@gmail.com>
2015-02-10 15:19:19 -0800

    restaurant_revenue_project
    In this first project using objects, I’ve created a short program that
    projects gross revenue for a restaurant based on several
    characteristics of the eatery.

---
+++ F1.js
@@ -0,0 +1,6 @@
+var name;
+name = prompt("Hello world"); //imput storing
+var message = "Ahoy there, " + name + "!" //'concatenation' (+ sign) puts two things together
+alert(message); //output
+
+//Demo Time
---
+++ F1_N_Feb2.js
@@ -0,0 +1,16 @@
+<script>
+var theSteps = function(input1, input2) {
+  //Steps to follow
+  return result;
+};
+
+
+</script>
+
+<script>
+  var findDifference = functions( answer, guess) /*answer and guess are defined her as local varialbes */ { 
+    if (answer > guess) {
+      return answer - guess;
+    };
+  }
+  </script>
